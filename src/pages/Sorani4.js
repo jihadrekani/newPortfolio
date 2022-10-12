d56fe577cfbb3f778354c367f42dca4f81b7283d
jihadrekani <jihadfed95@gmail.com>
2022-10-12 16:39:23 +0300

    add pola 6 sorani books

--- src/pages/Sorani4.js
+++ src/pages/Sorani4.js
@@ -17,7 +17,7 @@ export default function Sorani4() {
   return (
     <div className="">
       <h2 className="text-center xl:text-3xl lg:text-3xl md:text-3xl sm:text-2xl xs  font-bold text-3xl mt-[50px] mb-5 text-sky-900">
-      بنەڕەتى پۆلى یەکەم      </h2>
+      بنەڕەتى پۆلى چوارەم       </h2>
 
       <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
 
--- src/pages/Sorani6.js
+++ src/pages/Sorani6.js
@@ -1,7 +1,224 @@
-import React from 'react'
+import React from "react";
+import birkaripart1 from '../images/SeretayiSorani/pola6/birkaripart1.jpg'
+import birkaripart2 from '../images/SeretayiSorani/pola6/birkaripart2.jpg'
+import ayin from '../images/SeretayiSorani/pola6/ayin.jpg'
+import englishactivity from '../images/SeretayiSorani/pola6/englishactivity.jpg'
+import studentbook from '../images/SeretayiSorani/pola6/studentbook.jpg'
+import kurdi from '../images/SeretayiSorani/pola6/kurdi.jpg'
+import zanstpart1 from '../images/SeretayiSorani/pola6/zanstpart1.jpg'
+import zanstpart2 from '../images/SeretayiSorani/pola6/zanstpart2.jpg'
+import komelayeti from '../images/SeretayiSorani/pola6/komelayeti.jpg'
+
+
+
 
 export default function Sorani6() {
   return (
-    <div>Sorani6</div>
-  )
+    <div className="">
+      <h2 className="text-center xl:text-3xl lg:text-3xl md:text-3xl sm:text-2xl xs  font-bold text-3xl mt-[50px] mb-5 text-sky-900">
+      بنەڕەتى پۆلى شەشەم      </h2>
+
+      <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
+
+        <div className="rounded ">
+          <div className="flex justify-center ">
+            <div className="rounded-lg shadow-lg max-w-sm bg-gray-900">
+            <p  className="bg-gray-300 rounded-t-md py-2 text-center ">   زانست بۆ هەمووان - به‌رگى يه‌كه‌م      </p>
+
+                <img
+                  className="p-2 h-[400px]"
+                  src={zanstpart1}
+                  alt=""
+                />
+              <div className="p-6 flex items-center justify-center">
+                <a href="https://www.bgprogram.org/download.aspx?id=1065" 
+                
+                  className=" inline-block px-6 py-2.5 bg-orange-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-300 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>  
+        {/* ------------------------------------------------- */}
+<div className="rounded ">
+          <div className="flex justify-center ">
+            <div className="rounded-lg shadow-lg max-w-sm bg-gray-900">
+                <p  className="bg-gray-300 rounded-t-md py-2 text-center">  زانست بو هەمووان ـ بەرگی دووەم  </p>
+                <img
+                  className="p-2 h-[400px]"
+                  src={zanstpart2}
+                  alt=""
+                />
+              <div className="p-6 flex items-center justify-center">
+                <a href="https://www.bgprogram.org/download.aspx?id=1066" 
+                
+                  className=" inline-block px-6 py-2.5 bg-orange-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-300 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+{/* ------------------------------------------------- */}
+<div className="rounded ">
+          <div className="flex justify-center ">
+            <div className="rounded-lg shadow-lg max-w-sm bg-gray-900">
+                <p  className="bg-gray-300 rounded-t-md py-2 text-center"> بیرکاری بو هەمووان بەرگی یەکەم </p>
+                <img
+                  className="p-2 h-[400px]"
+                  src={birkaripart1}
+                  alt=""
+                />
+              <div className="p-6 flex items-center justify-center">
+                <a href="https://www.bgprogram.org/download.aspx?id=1062" 
+                
+                  className=" inline-block px-6 py-2.5 bg-orange-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-300 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+{/* ------------------------------------------------- */}
+<div className="rounded ">
+          <div className="flex justify-center ">
+            <div className="rounded-lg shadow-lg max-w-sm bg-gray-900">
+                <p  className="bg-gray-300 rounded-t-md py-2 text-center"> بیرکاری بو هەمووان بەرگی دووەم  </p>
+                <img
+                  className="p-2 h-[400px]"
+                  src={birkaripart2}
+                  alt=""
+                />
+              <div className="p-6 flex items-center justify-center">
+                <a href="https://www.bgprogram.org/download.aspx?id=1063" 
+                
+                  className=" inline-block px-6 py-2.5 bg-orange-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-300 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+{/* ------------------------------------------------- */}
+        <div className="rounded ">
+          <div className="flex justify-center ">
+            <div className="rounded-lg shadow-lg max-w-sm bg-gray-900">
+                <p  className="bg-gray-300 rounded-t-md py-2 text-center">  English - Activity</p>
+                <img
+                  className="p-2 h-[400px]"
+                  src={englishactivity}
+                  alt=""
+                />
+              <div className="p-6 flex items-center justify-center">
+                <a href="https://www.bgprogram.org/download.aspx?id=983" 
+                
+                  className=" inline-block px-6 py-2.5 bg-orange-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-300 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>      
+{/* ------------------------------------------------- */}
+        <div className="rounded ">
+          <div className="flex justify-center ">
+            <div className="rounded-lg shadow-lg max-w-sm bg-gray-900">
+                <p  className="bg-gray-300 rounded-t-md py-2 text-center">  Student's Book</p>
+                <img
+                  className="p-2 h-[400px]"
+                  src={studentbook}
+                  alt=""
+                />
+              <div className="p-6 flex items-center justify-center">
+                <a href="https://www.bgprogram.org/download.aspx?id=982" 
+                
+                  className=" inline-block px-6 py-2.5 bg-orange-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-300 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+{/* ------------------------------------------------- */}
+<div className="rounded ">
+          <div className="flex justify-center ">
+            <div className="rounded-lg shadow-lg max-w-sm bg-gray-900">
+                <p  className="bg-gray-300 rounded-t-md py-2 text-center">خوێندنى کوردى</p>
+                <img
+                  className="p-2 h-[400px]"
+                  src={kurdi}
+                  alt=""
+                />
+              <div className="p-6 flex items-center justify-center">
+                <a href="https://www.bgprogram.org/download.aspx?id=1064" 
+                
+                  className=" inline-block px-6 py-2.5 bg-orange-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-300 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+{/* ------------------------------------------------- */}
+<div className="rounded ">
+          <div className="flex justify-center ">
+            <div className="rounded-lg shadow-lg max-w-sm bg-gray-900">
+                <p  className="bg-gray-300 rounded-t-md py-2 text-center">  په‌روه‌رده‌ى ئيسلامى</p>
+                <img
+                  className="p-2 h-[400px]"
+                  src={ayin}
+                  alt=""
+                />
+              <div className="p-6 flex items-center justify-center">
+                <a href="https://www.bgprogram.org/download.aspx?id=890" 
+                
+                  className=" inline-block px-6 py-2.5 bg-orange-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-300 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+{/* ------------------------------------------------- */}
+<div className="rounded ">
+          <div className="flex justify-center ">
+            <div className="rounded-lg shadow-lg max-w-sm bg-gray-900">
+                <p  className="bg-gray-300 rounded-t-md py-2 text-center">    بابه‌ته‌ كۆمه‌لايه‌تييه‌كان  </p>
+                <img
+                  className="p-2 h-[400px]"
+                  src={komelayeti}
+                  alt=""
+                />
+              <div className="p-6 flex items-center justify-center">
+                <a href="https://www.bgprogram.org/download.aspx?id=1067" 
+                
+                  className=" inline-block px-6 py-2.5 bg-orange-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-300 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+      </div>
+    </div>
+  );
 }
